019cf9a5821fe93499c2d12cbc338eb90565b299
Gumbers <gustavomarinsuppi@gmail.com>
2020-05-25 19:05:16 -0300

    feat: click once time on down key after last obstacle passad

--- Javascript/trex_google/game.js
+++ Javascript/trex_google/game.js
@@ -3,6 +3,8 @@ var space = new KeyboardEvent('keydown', { keyCode: 32 })
 var down = new KeyboardEvent('keydown', { keyCode: 40 })
 var updown = new KeyboardEvent('keyup', { keyCode: 40 })
 
+
+ var lastDown = false
 var method = function () {
 	var gameSpeed = this.window.Runner.instance_.currentSpeed
 
@@ -16,25 +18,34 @@ var method = function () {
 	var nextObs = obstacles.find(
 		x => x.xPos - trex.xPos > 0 && x.xPos - trex.xPos < 20 * gameSpeed
 	)
+	
+	var oldObject = obstacles.find(x => x.xPos - trex.xPos < 0)
 
 	if (nextObs) {
 		if (nextObs.typeConfig.type === 'PTERODACTYL') {
 			if (nextObs.yPos === 100) {
+				lastDown = false
 				document.dispatchEvent(updown)
 				document.dispatchEvent(up)
 				console.log('up', nextObs, trex)
 			} else {
+				lastDown = true
 				document.dispatchEvent(down)
 				console.log('down', nextObs, trex)
 			}
 		} else {
+			lastDown =false
 			document.dispatchEvent(updown)
 			document.dispatchEvent(up)
 			console.log('up', nextObs, trex)
 		}
 	}else{
+		if(!oldObject && !lastDown ){
+			
+		lastDown = true
     document.dispatchEvent(down)
   }
+}
 }
 
 setInterval(method, 15)
