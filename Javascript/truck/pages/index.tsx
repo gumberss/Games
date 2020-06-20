6322e3ff7f08a9b3a7f76767452188eb7d505f7d
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-20 10:02:54 -0300

    feat: adjust canvas size and cycle of the game

--- Javascript/truck/pages/index.tsx
+++ Javascript/truck/pages/index.tsx
@@ -1,33 +1,47 @@
-import Head from 'next/head'
 import { useEffect, useRef } from 'react'
 import renderScreen from '../public/render-screen'
 
 export default function Home() {
-  const canvasRef = useRef<HTMLCanvasElement>()
+	const canvasRef = useRef<HTMLCanvasElement>()
 
 	useEffect(() => {
+		const canvas = canvasRef.current!
 
-		const canvas = canvasRef.current!;
-		
-		const context = canvas.getContext("2d") 
+		const context = canvas.getContext('2d')!
 
-    canvas.width = 80
-    canvas.height = 40
+		canvas.width = 120
+		canvas.height = 60
 
-    context!.fillStyle = 'white'
-    //context.clearRect(0, 0, game.state.screen.width, game.state.screen.height)
-    renderScreen(context, canvas)
-  })
+		context!.fillStyle = 'white'
+		//context.clearRect(0, 0, game.state.screen.width, game.state.screen.height)
+		//renderScreen(context, canvas)
+		tick(canvas, context, {}, requestAnimationFrame)
+	})
 
-	//@ts-ignore
-	return <canvas id="screen" ref={canvasRef} style={{
-		height: '500px',
-		width: '1000px',
-		border: '1px solid #ccc',
-		imageRendering: 'pixelated',
-	}}></canvas>
-
-
+	return (
+		<canvas
+			id="screen"
+			//@ts-ignore
+			ref={canvasRef}
+			style={{
+				height: '500px',
+				width: '1000px',
+				border: '1px solid #ccc',
+				imageRendering: 'pixelated',
+			}}
+		></canvas>
+	)
 }
 
+const tick = (
+	canvas: HTMLCanvasElement,
+	context: CanvasRenderingContext2D,
+	game: any,
+	requestAnimationFrame: any
+) => {
+	requestAnimationFrame(() => {
+		renderScreen(context, canvas)
 
+		tick(canvas, context, game, requestAnimationFrame)
+	})
+}
